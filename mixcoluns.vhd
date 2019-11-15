e47e707c612c4519eb30a909801a7332ba6e3b29
marcelo-cordeiro <50764613+marcelo-cordeiro@users.noreply.github.com>
2019-11-15 04:41:13 -0300

    Add files via upload

---
+++ mixcoluns.vhd
@@ -0,0 +1,35 @@
+-- Quartus II VHDL Template
+-- Unsigned Adder
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity mixcoluns is
+
+	port 
+	(
+		men_in: in std_logic_vector (127 downto 0);
+		men_out: out std_logic_vector (127 downto 0)
+	);
+
+end entity;
+
+
+architecture rtl of mixcoluns is
+component mult2 is
+
+	port 
+	(
+		men_in : in std_logic_vector(7 downto 0);
+		men_out: out std_logic_vector(7 downto 0)
+	);
+
+end component;
+signal entradaMult2, saidaMult2: std_logic_vector (7 downto 0);
+begin
+
+p1: mult2 port map (men_in <= entradaMult2, men_out <= saidaMult2);
+
+
+end rtl;
---
+++ mult2.vhd
@@ -0,0 +1,29 @@
+-- Quartus II VHDL Template
+-- Unsigned Adder
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity mult2 is
+
+	port 
+	(
+		men_in : in std_logic_vector(7 downto 0);
+		men_out: out std_logic_vector(7 downto 0)
+	);
+
+end entity;
+
+architecture rtl of mult2 is
+signal entrada1,saida,auxSaida: std_logic_vector(8 downto 0);
+begin
+	entrada1 <= men_in(31 downto 24) & '0';
+		if(entrada1(8)='1') then
+			auxSaida <= entrada1 xor "100011011";
+			saida <= auxSaida(7 downto 0);
+		else
+			saida <= entrada1(7 downto 0);
+		end if;
+		men_out <= saida;
+end rtl;
